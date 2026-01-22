f4fec88deeac5b4055575b9977e260a4515bdfb0
benja2998 <benja2998@duck.com>
2026-01-21 23:56:55 -0300

    update

---
+++ src/addShebang.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const path = require("path");
+
+const shebang = "#!/usr/bin/env node\n";
+
+const files = [
+	path.join(__dirname, "../dist/index.js"),
+]
+
+files.forEach(file => {
+	if (!fs.existsSync(file)) {
+		console.warn(`File not found: ${file}`);
+		return;
+	}
+
+	const content = fs.readFileSync(file, 'utf8');
+
+	// Only add shebang if it doesn't already exist
+	if (!content.startsWith('#!')) {
+		fs.writeFileSync(file, shebang + content, 'utf8');
+		console.log(`Shebang added to ${file}`);
+	} else {
+		console.log(`Shebang already exists in ${file}`);
+	}
+});
